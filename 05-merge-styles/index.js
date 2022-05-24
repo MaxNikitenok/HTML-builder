ccba180dea742ae18d7f0f6bf67016322745e4c3
MaxNikitenok <xxx250790@gmail.com>
2022-05-24 22:05:36 +0300

    fix: fixing rm in task 05-mergr-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,16 +4,14 @@ const stylesDir = path.join(__dirname, 'styles');
 const projectDir = path.join(__dirname, 'project-dist', 'bundle.css');
 const writeStream = fs.createWriteStream(projectDir);
 
-fs.rm(projectDir, () => {
-  fs.writeFile(projectDir, '', () => {
-    fs.readdir(stylesDir, function(err, items) {
-      items.forEach(item => {
-        if(item.split('.')[1] === 'css') {
-          fs.readFile(path.join(stylesDir, item), (err, data) => {
-            writeStream.write(`${data}\n`);
-          });
-        }
-      });
+fs.writeFile(projectDir, '', () => {
+  fs.readdir(stylesDir, function(err, items) {
+    items.forEach(item => {
+      if(item.split('.')[1] === 'css') {
+        fs.readFile(path.join(stylesDir, item), (err, data) => {
+          writeStream.write(`${data}\n`);
+        });
+      }
     });
   });
 });
