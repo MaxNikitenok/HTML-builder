d4b6ed6d4dd437ff26bff34cc0d476c104b50815
MaxNikitenok <xxx250790@gmail.com>
2022-05-24 10:34:04 +0300

    refactor: refactoring task 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,48 +3,50 @@ const path = require('path');
 const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
 let flag = 0;
 
-rm();
-
-fs.mkdir(path.join(__dirname, 'project-dist'), () => {
-  fs.copyFile(path.join(__dirname, 'template.html'), path.join(__dirname, 'project-dist', 'index.html'), () => {
-  });
-});
-/*---------render-index-------*/
-fs.readdir(path.join(__dirname, 'components'), (err, items) => {
-  let components = [];
-  items.forEach(item => {
-    if(path.extname(item) === '.html') {
-      components.push(item);
-    }
-  });
-  render(components[flag]);
-  function render(component) {
-    flag += 1;
-    fs.readFile(path.join(__dirname, 'components', component), (_err, file) => {
-      fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 'utf8', function (_err,data) {
-        let result = data.replace(`{{${component.split('.')[0]}}}`, `${file.toString()}`);
-        fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, 'utf8', () => {
-          if(items.length > flag) {
-            render(components[flag]);
-          }
-        });
-      });
+function addBase() {
+  fs.mkdir(path.join(__dirname, 'project-dist'), () => {
+    fs.copyFile(path.join(__dirname, 'template.html'), path.join(__dirname, 'project-dist', 'index.html'), () => {
     });
-  }
-});
-/*----------------style-------------------- */
-fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', () => {
-  fs.readdir(path.join(__dirname, 'styles'), (err, items) => {
+  });
+  /*---------render-index-------*/
+  fs.readdir(path.join(__dirname, 'components'), (err, items) => {
+    let components = [];
     items.forEach(item => {
-      if(item.split('.')[1] === 'css') {
-        fs.readFile(path.join(path.join(__dirname, 'styles'), item), (err, data) => {
-          writeStream.write(`${data}\n`);
-        });
+      if(path.extname(item) === '.html') {
+        components.push(item);
       }
     });
+    render(components[flag]);
+    function render(component) {
+      flag += 1;
+      fs.readFile(path.join(__dirname, 'components', component), (_err, file) => {
+        fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 'utf8', function (_err,data) {
+          let result = data.replace(`{{${component.split('.')[0]}}}`, `${file.toString()}`);
+          fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, 'utf8', () => {
+            if(items.length > flag) {
+              render(components[flag]);
+            }
+          });
+        });
+      });
+    }
   });
-});
-/*----------------rm-add-------------------- */
+}
+
+function addStyle() {
+  fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', () => {
+    fs.readdir(path.join(__dirname, 'styles'), (err, items) => {
+      items.forEach(item => {
+        if(item.split('.')[1] === 'css') {
+          fs.readFile(path.join(path.join(__dirname, 'styles'), item), (err, data) => {
+            writeStream.write(`${data}\n`);
+          });
+        }
+      });
+    });
+  });
+}
+
 function rm(){
   fs.stat(path.join(__dirname, 'project-dist', 'assets'), function(err) {
     if (!err) {
@@ -53,23 +55,17 @@ function rm(){
           fs.readdir(path.join(__dirname, 'project-dist', 'assets', dir), (err, files) => {
             files.forEach(file => {
               fs.rm(path.join(__dirname, 'project-dist', 'assets', dir, file), () =>{
-                console.log('удален ' + file);
               });
             });
-            ass();
+            addAss();
           });
         });
       });
     }
-    else if (err.code === 'ENOENT') {
-      fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), () => {
-        ass();
-      });
-    }
   });
 }
-/*-------------assets         */
-function ass() {
+
+function addAss() {
   fs.readdir(path.join(__dirname, 'assets'), function(err, items) {
     items.forEach(item => {
       fs.stat(path.join(__dirname, 'assets', item), function(err, stats) {
@@ -79,7 +75,6 @@ function ass() {
           fs.readdir(path.join(__dirname, 'assets', item), function(err, files) {
             files.forEach(file => {
               fs.copyFile(path.join(__dirname, 'assets', item, file), path.join(__dirname, 'project-dist', 'assets', item, file), () => {
-                console.log('скопированн ' + file);
               });
             });
           });
@@ -88,4 +83,6 @@ function ass() {
     });
   });
 }
-
+rm();
+addBase();
+addStyle();
